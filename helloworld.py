dfb95abdad59837ddb4b7917016b07e69961d68c
lydia <kerrly@wgc.school.nz>
2019-02-15 10:01:30 +1300

    configured colours and fonts

--- helloworld.py
+++ helloworld.py
@@ -5,15 +5,16 @@ root.title("Hello World!!!")
 root.geometry("600x200")
 
 my_label = tkinter.Label(root)
-my_label.config(text= "Hello dickhead!!", fg="blue")
+my_label.config(text= "R U Real??!!", fg="blue", bg="violet", font=("Times", "100"))
 my_label.grid()
 
 my_label2 = tkinter.Label(root)
-my_label2.config(text= "sup bro", fg="orange")
+my_label2.config(text= "ooft", fg="orange", bg="hot pink", font=("Bookman", "50"), padx="20")
 my_label2.grid()
 
 my_label3 = tkinter.Label(root)
-my_label3.config(txt= "yeaa nahhh", fg="red")
+my_label3.config(text= "ruelle", fg="pink", bg="green", font=("Garamond", "80"))
 my_label3.grid()
+
 
 root.mainloop()
