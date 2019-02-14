b85111fbb2abd5c2555a67b0feadea9846ebdbab
lydia <kerrly@wgc.school.nz>
2019-02-15 10:11:51 +1300

    initial commit

---
+++ ooft.py
@@ -0,0 +1,11 @@
+import tkinter
+root = tkinter.Tk()
+
+def print_ooft():
+    prnt("ooft")
+
+button1 = tkinter.Button(root)
+button1.config(text="mansfield pride", bg="teal", font="Bookman" "100",  width="50", height="50")
+button1.grid()
+
+root.mainloop()
