b0b7c14c5afb7327e59580bb2f70b42c77359457
lydia <kerrly@wgc.school.nz>
2019-02-14 15:12:35 +1300

    added my_label lines X 3

--- helloworld.py
+++ helloworld.py
@@ -4,4 +4,8 @@ root = tkinter.Tk()
 root.title("Hello World!!!")
 root.geometry("600x200")
 
+my_label = tkinter.Label(root)
+my_label.config(text= "Hello dickhead!!", fg="blue")
+my_label.grid()
+
 root.mainloop()
