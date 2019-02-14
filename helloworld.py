8ad241b8c213d7fbaa25ef206cf2ec0363d7a072
lydia <kerrly@wgc.school.nz>
2019-02-14 15:16:44 +1300

    added my_label2 and my_label3 lines X 3 each

--- helloworld.py
+++ helloworld.py
@@ -8,4 +8,12 @@ my_label = tkinter.Label(root)
 my_label.config(text= "Hello dickhead!!", fg="blue")
 my_label.grid()
 
+my_label2 = tkinter.Label(root)
+my_label2.config(text= "sup bro", fg="orange")
+my_label2.grid()
+
+my_label3 = tkinter.Label(root)
+my_label3.config(txt= "yeaa nahhh", fg="red")
+my_label3.grid()
+
 root.mainloop()
